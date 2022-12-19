8a4316f5f0cb4f846329662a36eadbeae20c75cb
Aldot-02 <twizald.02@gmail.com>
2022-12-19 02:05:44 -0800

    First nine tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes the pointer and updates the value
+ * @n: int to check
+ * owned By Aldot
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
