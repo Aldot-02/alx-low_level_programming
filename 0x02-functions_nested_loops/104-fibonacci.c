2c99e79669019619a55ec6940cf3f99c0203a250
Aldot-02 <twizald.02@gmail.com>
2022-12-23 02:12:50 -0800

    Unsolvable task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -24,7 +24,6 @@ int main(void)
 		fib2 = sum;
 
 	}
-
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
@@ -39,7 +38,6 @@ int main(void)
 			half1 += 1;
 			half2 %= 10000000000;
 		}
-
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
 			printf(", ");
